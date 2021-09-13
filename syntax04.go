dd156e3c3e6753707e4765b214322d1079153389
GrO0vy <lmc2819@naver.com>
2021-09-14 02:07:33 +0900

    syntax04_v2.go

--- syntax04.go
+++ syntax04.go
@@ -1,12 +1,10 @@
-package main
-
 import (
 	"fmt"
 	"math/rand"
 	"time"
 )
 
-// 난수 추출된 수의 소수 판정 프로그램 ver 0.1
+// 난수 추출된 수의 소수 판정 프로그램 ver 0.2
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
@@ -15,9 +13,9 @@ func main() {
 	number := rand.Intn(150) + 2 // 0 과 1 제외
 	fmt.Println("임의로 추출된 수 : ", number)
 
-	for i := 1; i <= number; i++ {
-		if number%i == 0 {
-			count++
+	for i := 2; i < number; i++ {
+		if number%i == 0 { // 1과 number 일 때 loop를 돌지 않음
+			count = count + 1
 		}
 	}
 	if count == 2 {
