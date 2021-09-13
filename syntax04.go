ac45fbcc95a9f613e49ac692c7e444cf8ca539b6
GrO0vy <lmc2819@naver.com>
2021-09-14 02:08:27 +0900

    syntax04_v4.go

--- syntax04.go
+++ syntax04.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-// 난수 추출된 수의 소수 판정 프로그램 ver 0.3
+// 난수 추출된 수의 소수 판정 프로그램 ver 0.4
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
@@ -19,8 +19,9 @@ func main() {
 		if number%i == 0 { // 1과 number 일 때 loop를 돌지 않음
 			isPrime = false
 		}
+		fmt.Print(i, " ")
 	}
-	if isPrime == true {
+	if isPrime { // 비교연산 제거
 		fmt.Printf("%d는 소수입니다.", number)
 	} else {
 		fmt.Printf("%d는 소수가 아닙니다.", number)
